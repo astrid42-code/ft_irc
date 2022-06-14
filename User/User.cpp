e296d8980665d0b218657ffd4414a01a14eb3b09
Astrid Gaultier <astrid@Gilless-iMac.local>
2022-06-14 19:56:38 +0200

    user 2

--- User/User.cpp
+++ User/User.cpp
@@ -12,7 +12,9 @@
 
 #include "User.hpp"
 
-namespace irc{
+namespace irc
+{
+//}
 	
 User::User(){
 	// parser avec regles user (voir notes)
@@ -24,4 +26,40 @@ User::~User(){
 	// delete les differents users	
 }
 
+User & operator=(const User & user_cp){
+	_name = user_cp._name;
+	return (*this);
+}
+
+std::string User::get_user() const{
+	return (_name);
+}
+
+std::string User::get_pwd() const{
+	return (_pwd);
+}
+
+void	User::set_user(std::string name){
+	_name = name;
+}
+
+void	User::set_pwd(std::string pwd){
+	_pwd = pwd;
+}
+
+// a faire avant de stocker le user dans une map/un vector
+bool	check_user(){
+	if (_user.size() > 9){
+		std::cout << "Login is too long : 9 characters max" << std::endl; 
+		return (false);
+	}
+	// comparer ensuite avec les autres logins déjà connectés :
+	// msg  a l'inscription en cas de doublon 
+	// std::cout << "*** Nickname " << _name << "" is already in use, type /nick newnick to use another." << std::endl;
+	// accepte le login doublon mais possibilite de changer avec la cmd donnee
+	// donc prévoir de modifier le vector (et la map dans laquelle il est stocké?) 
+	// donc pas à faire ici mais à lancer dans une autre fct externes (dans le server?)
+
+	return (true);
+}
 }
--- User/User.hpp
+++ User/User.hpp
@@ -11,9 +11,20 @@ namespace irc{
 			User();
 			~User();
 
+			User & operator=(const User & user_cp);
+
+			// getters, setters
+			std::string 	get_user() const;
+			std::string 	get_pwd() const;
+			void			set_user(std::string name);
+			void			set_pwd(std::string pwd);
+
+			bool	check_user();
+
 
 		private:
-
+			std::string _name;
+			std::string _pwd;
 	};
 }
 
