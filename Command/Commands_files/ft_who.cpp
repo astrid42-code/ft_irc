ac3b7fbec94169f4d062d7720d80f64799185cb7
Tadeo AMIGORENA <tamigore@e1r12p14.clusters.42paris.fr>
2022-09-27 14:52:25 +0200

    who unbuged

--- Command/Commands_files/ft_who.cpp
+++ Command/Commands_files/ft_who.cpp
@@ -59,9 +59,9 @@ void	who(Cmd &command)
 		{
 			if (command.get_size() >= 1 && command.get_value()[1][0] != '0' && command.get_value()[1].compare("*") != 0)
 			{
-				if (mask_off(command.get_value()[1], itu->second->get_hostname()) || mask_off(command.get_value()[1], itu->second->get_name()) || mask_off(command.get_value()[1], SERVER))
+				if (mask_off(command.get_value()[0], itu->second->get_hostname()) || mask_off(command.get_value()[0], itu->second->get_name()) || mask_off(command.get_value()[0], SERVER))
 				{
-					if (command.get_size() == 1 || (command.get_size() == 2 && command.get_value()[2][0] == 'o' && itu->second->find_mod("o")))
+					if (command.get_size() == 1 || (command.get_size() == 2 && command.get_value()[1][0] == 'o' && itu->second->find_mod("o")))
 					{
 						command._server->send_msg(RPL_WHOREPLY(command._user->get_hostname(), itu->second->get_channel_name(), itu->second->get_user(), itu->second->get_host(), itu->second->get_nick(), itu->second->get_name()), command._sfd);
 						match = true;
